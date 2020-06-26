d24a17ad08ea27e64b79eabb88032fd5ca6c78f9
Aslak Hollund <aslak.hollund@nav.no>
2020-06-26 15:57:03 +0200

    Updates

---
+++ precache-manifest.67382627f2a54f5eeec47e023e5bda29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4899f075be103f21b9e0637b14bc7f3",
+    "url": "/aslakhol.github.io/infoskjerm-test/index.html"
+  },
+  {
+    "revision": "ae4f757a70ee4361c7ff",
+    "url": "/aslakhol.github.io/infoskjerm-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "efff5db2b7a3e61cd1f5",
+    "url": "/aslakhol.github.io/infoskjerm-test/static/js/2.5c29b3f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aslakhol.github.io/infoskjerm-test/static/js/2.5c29b3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4f757a70ee4361c7ff",
+    "url": "/aslakhol.github.io/infoskjerm-test/static/js/main.b17d0886.chunk.js"
+  },
+  {
+    "revision": "a8ed19f961816fb287c2",
+    "url": "/aslakhol.github.io/infoskjerm-test/static/js/runtime-main.c54e23df.js"
+  }
+]);
